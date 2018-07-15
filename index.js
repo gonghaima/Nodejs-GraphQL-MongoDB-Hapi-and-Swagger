6a514fec25dc36e6cba372f4418c991465bbd874
steven gong <gonghaima@hotmail.com>
2018-07-15 10:46:56 +1000

    add response for GET request

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const server = hapi.server({
 });
 
 const init = async() => {
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function(request, reply) {
+            return `<h1>My modern api</h1>`
+        }
+    });
     await server.start();
     console.log(`Server running at: ${server.info.uri}`);   
 };
