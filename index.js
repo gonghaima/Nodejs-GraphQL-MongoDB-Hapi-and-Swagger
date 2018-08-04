8cfe3eb88979d9924a5041d8a45d46639da9e97e
steven <gonghaima@hotmail.com>
2018-08-04 12:39:07 +1000

    mongoose schema added & post data to mongo server-mLab

--- index.js
+++ index.js
@@ -6,21 +6,43 @@ const server = hapi.server({
     host: 'localhost'
 });
 
-const init = async() => {
 
+const init = async () => {
     mongoose.connect('mongodb://whoisyourmum:8whoisyourmum@ds119171.mlab.com:19171/hapigraphql');
-    mongoose.connection.once('open', ()=>{
-        console.log('connected to database'); 
+    mongoose.connection.once('open', () => {
+        console.log('connected to database');
     });
-    server.route({
+    const Painting = require('./models/Painting');
+    server.route([{
         method: 'GET',
         path: '/',
-        handler: function(request, reply) {
+        handler: function (request, reply) {
             return `<h1>My modern api</h1>`
         }
-    });
+    },
+    {
+        method: 'GET',
+        path: '/api/v1/paintings',
+        handler: (req, reply) => {
+            return Painting.find();
+        }
+    },
+    {
+        method: 'POST',
+        path: '/api/v1/paintings',
+        handler: (req, reply) => {
+            const { name, url, techniques } = req.payload;
+            const painting = new Painting({
+                name,
+                url,
+                techniques
+            });
+
+            return painting.save();
+        }
+    }]);
     await server.start();
-    console.log(`Server running at: ${server.info.uri}`);   
+    console.log(`Server running at: ${server.info.uri}`);
 };
 
 init();
